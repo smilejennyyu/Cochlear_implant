6f03c7a4dd743de0b85b2f4613be9d36d0aeeabb
MaggieXJZhang <xujiazhang310@gmail.com>
2019-06-09 12:28:52 -0400

    Final copy of phase 1

--- phase1.m
+++ phase1.m
@@ -23,24 +23,27 @@ audiowrite(output_file, raw_data_mono, sample_rate);
 stop_time = size(raw_data_mono)/sample_rate;
 time_step = 1/sample_rate;
 t = time_step:time_step:stop_time;
+sample_number = 1:1:size(raw_data_mono);
 %t = 1:1:size(raw_data_mono);
 
-disp(strcat('raw_data_mono size: ', string(size(raw_data_mono))));
-disp(strcat('t size: ', string(size(t))));
-figure('Name', 'Raw Data Mono');
-%plot(t, raw_data_mono);
+%disp(strcat('raw_data_mono size: ', string(size(raw_data_mono))));
+%disp(strcat('t size: ', string(size(t))));
+figure('Name', 'Raw Data Mono vs. Sample Number');
+plot(sample_number, raw_data_mono,'g');
+title('3.5 Mono Sound Waveform(Amplitude vs. Sample Number)');
 
 % 3.6 If sampling rate is greater than 16k, downsample it
 data_16k = resample(raw_data_mono, 16000, sample_rate);
-disp(strcat('data_16k size: ', string(size(data_16k))));
+%disp(strcat('data_16k size: ', string(size(data_16k))));
 rate_16k = 16000;
 stop_time_16k = size(data_16k)/rate_16k;
 t_16 = 1/rate_16k:1/rate_16k:stop_time_16k;
-plot(t(1:2750), raw_data_mono(1:2750),'g');
-figure('Name', 'Raw Data Resampled');
-plot(t_16(1:1000), data_16k(1:1000),'r');
-figure('Name', 'Data Comparison');
-plot(t_16(1:1000), data_16k(1:1000),'r',t(1:2750), raw_data_mono(1:2750),'g');
+
+%figure('Name', 'Raw Data Resampled');
+%plot(t_16(1:1000), data_16k(1:1000),'r');
+%sound(data_16k, rate_16k);
+%figure('Name', 'Data Comparison');
+%plot(t_16(1:1000), data_16k(1:1000),'r',t(1:2750), raw_data_mono(1:2750),'g');
 
 
 % 3.7 Generate a signal using 1kHz cosine function that has the same time
@@ -48,9 +51,10 @@ plot(t_16(1:1000), data_16k(1:1000),'r',t(1:2750), raw_data_mono(1:2750),'g');
 freq = 1000;
 cos_1kHz = cos(2000*pi*t);
 sound(cos_1kHz, freq);
-disp(t(size(t)));
-figure('Name', 'Two Waveform of Cosine Function');
+%disp(t(size(t)));
 cycle_plot = 2;
 time_plot = cycle_plot/freq;
+figure('Name', 'Two Waveforms of Cosine Function');
 plot(t(1:time_plot*sample_rate),(cos_1kHz(1:time_plot*sample_rate)) );
-%sound(raw_data_mono, sample_rate);
+title('3.7 Two Waveforms of Cosine(Amplitude vs. Time)');
+
