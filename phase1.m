8bd1ae2064d1678a01f87d6965d4e5c6f1f60757
Xujia(Maggie) Zhang <37022534+MaggieXJZhang@users.noreply.github.com>
2019-06-09 10:57:41 -0400

    Cleaned up the code and changed the sound file to the smaller one

--- phase1.m
+++ phase1.m
@@ -1,16 +1,25 @@
-% Create program to read the file
+% 3.1 Create program to read the file
 filename = 'Khan_Girl.wav';
 [raw_data,sample_rate] = audioread(filename);
+
+% 3.2 Check if input sound is stereo
 [m,n] = size(raw_data);
-%check if input sound is stereo
-if n > 1
+if n > 1 % If stereo:
+    % Combine two channels into one and take average
     raw_data_mono = sum(raw_data,2) / size(raw_data,2);
     %disp(raw_data_mono);
 else
     raw_data_mono = raw_data;
 end
+
+% 3.3 Play the sound
+sound(raw_data_mono, sample_rate);
+
+% 3.4 Write sound to a new file 
 output_file = strcat('new_', filename);
 audiowrite(output_file, raw_data_mono, sample_rate);
+
+% 3.5 Plot sound wave as a function of sample number
 stop_time = size(raw_data_mono)/sample_rate;
 time_step = 1/sample_rate;
 %sound(raw_data_mono, sample_rate);
@@ -22,12 +31,13 @@ disp(size(t));
 figure('Name', 'Raw Data Mono');
 plot(t, raw_data_mono);
 
-
+% 3.6 If sampling rate is greater than 16k, downsample it
 data_16k = resample(raw_data_mono, 16000, sample_rate);
 rate_16k = 16000;
 t_16 = 1:1/rate_16k:size(data_16k)/rate_16k;
 %figure('Name', 'Raw Data resampled');
 plot(t_16, data_16k,'r',t, raw_data_mono,'g');
+
 
 
 %generate cos wave signal
